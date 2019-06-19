ea8617a56cd53597c059debc51f9929f547a5cc3
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-06-19 00:51:44 +0000

    first coomit

---
+++ lib/Daily/Study.rb
@@ -0,0 +1,8 @@
+require "Daily/Study/version"
+
+module Daily
+  module Study
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/Daily/Study/version.rb
@@ -0,0 +1,5 @@
+module Daily
+  module Study
+    VERSION = "0.1.0"
+  end
+end
